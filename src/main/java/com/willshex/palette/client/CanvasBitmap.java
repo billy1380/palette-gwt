1d06b5ff4f5d29f741a26336cb6c626e447243ac
billy1380 <william.shakour@gmail.com>
2015-06-24 18:14:44 +0100

    - changed invalid gwt module name - added CanvasBitmap initial version

---
+++ src/main/java/com/willshex/palette/client/CanvasBitmap.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2015 WillShex Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.willshex.palette.client;
+
+import com.google.gwt.canvas.client.Canvas;
+import com.google.gwt.canvas.dom.client.CanvasPixelArray;
+import com.willshex.palette.shared.Bitmap;
+import com.willshex.palette.shared.Color;
+
+public class CanvasBitmap implements Bitmap {
+
+  private Canvas canvas;
+  
+  @Override
+  public int getWidth() {
+    return canvas.getCoordinateSpaceWidth();
+  }
+
+  @Override
+  public int getHeight() {
+    return canvas.getCoordinateSpaceHeight();
+  }
+
+  @Override
+  public void getPixels(int[] pixels, int offset, int stride, int x, int y,
+      int width, int height) {
+    CanvasPixelArray array = canvas.getContext2d()
+        .getImageData(0, 0, width, height).getData();
+    // copy it to an array
+    int dataLength = array.getLength();
+    for (int i = 0; i < dataLength; i++) {
+      if (i % 4 == 0) {
+        pixels[i / 4] = Color.argb(array.get(i + 3), array.get(i),
+            array.get(i + 1), array.get(i + 2));
+      }
+    }
+  }
+
+  @Override
+  public Bitmap copyScaled(int width, int height) {
+    return null;
+  }
+
+  @Override
+  public void recycle() {
+    canvas.removeFromParent();
+  }
+
+  @Override
+  public boolean isRecycled() {
+    return !canvas.isAttached();
+  }
+
+}
